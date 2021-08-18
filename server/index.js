8a6ee45c56236a45d446221a74b97a294fbe4dd4
Harsh Vardhan Goel <harshvardhanmpsite@gmail.com>
2021-08-18 09:25:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
 const CONNECTION_URL =
-  "mongodb+srv://memories:harsh3108memories@cluster0.hwgzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "YOUR_CONNECTION_URL";
 const PORT = process.env.PORT || 5000;
 
 mongoose
